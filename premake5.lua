0ef27f198be4b6e4ae958c5a9a65b1599c878574
Erik Lauterwald <92889691+erikrl2@users.noreply.github.com>
2022-05-16 17:21:40 +0200

    add premake5.lua and modify .gitignore

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+
+workspace "Hazel"
+	architecture "x86_64"
+	startproject "Hazelnut"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	solution_items
+	{
+		".editorconfig"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["spdlog"] = "%{wks.location}/Hazel/vendor/spdlog/include"
+IncludeDir["GLFW"] = "%{wks.location}/Hazel/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/Hazel/vendor/Glad/include"
+IncludeDir["Imgui"] = "%{wks.location}/Hazel/vendor/imgui"
+IncludeDir["glm"] = "%{wks.location}/Hazel/vendor/glm"
+IncludeDir["stb_image"] = "%{wks.location}/Hazel/vendor/stb_image"
+IncludeDir["entt"] = "%{wks.location}/Hazel/vendor/entt/include"
+
+group "Dependencies"
+	include "vendor/premake"
+	include "Hazel/vendor/GLFW"
+	include "Hazel/vendor/Glad"
+	include "Hazel/vendor/imgui"
+group ""
+
+include "Hazel"
+include "Sandbox"
+include "Hazelnut"
